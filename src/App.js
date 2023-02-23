e97efa8c2d2fb34a4382973de71d37a8021fcc10
Arup <09arup06@gmail.com>
2023-02-23 20:11:27 +0530

    3rd commit

--- src/App.js
+++ src/App.js
@@ -17,14 +17,14 @@ export default class App extends Component {
         
         <Navbar/>
         <Routes>
-          <Route path="/sports" element={<News key="sports" pageSize={18} country="in" category="sports"/>}/>
-          <Route path="/science" element={<News key="science" pageSize={18} country="in" category="science" />}/>
-          <Route path="/technology" element={<News key="technology" pageSize={18} country="in" category="technology" />}/>
-          <Route path="/entertainment" element={<News key="entertainment" pageSize={18} country="in" category="entertainment" />}/>
-          <Route path="/health" element={<News key="health" pageSize={18} country="in" category="health" />}/>
-          <Route path="/" element={<News key="general" pageSize={18} country="in" category="general" />}/>
-          <Route path="/business" element={<News key="business" pageSize={18} country="in" category="business" />}/>
-          <Route path="/home" element={<News key="general" pageSize={18} country="in" category="general" />}/>
+          <Route exactpath="/sports" element={<News key="sports" pageSize={18} country="in" category="sports"/>}/>
+          <Route exact path="/science" element={<News key="science" pageSize={18} country="in" category="science" />}/>
+          <Route exact path="/technology" element={<News key="technology" pageSize={18} country="in" category="technology" />}/>
+          <Route exact path="/entertainment" element={<News key="entertainment" pageSize={18} country="in" category="entertainment" />}/>
+          <Route exact path="/health" element={<News key="health" pageSize={18} country="in" category="health" />}/>
+          <Route exact path="/News-Site" element={<News key="general" pageSize={18} country="in" category="general" />}/>
+          <Route exact path="/business" element={<News key="business" pageSize={18} country="in" category="business" />}/>
+          <Route exact path="/home" element={<News key="general" pageSize={18} country="in" category="general" />}/>
           <Route exact path="https://09arup06.github.io/News-Site/"element={<News key="general" pageSize={18} country="in" category="general" />}/>
           
         </Routes>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ export class navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a className="navbar-brand" href="/">Ebar Khobor</a>
+        <a className="navbar-brand" href="/home">Ebar Khobor</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -13,7 +13,7 @@ export class navbar extends Component {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item">
-              <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
+              <Link className="nav-link" to="/home">Home <span className="sr-only">(current)</span></Link>
             </li>
           
             <li className="nav-item">
